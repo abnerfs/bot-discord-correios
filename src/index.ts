7cb4e6d7269e6144ee543f1018cab70c6a9ea11d
abnerfs <abnerfs@gmail.com>
2020-05-04 19:59:13 -0300

    Interval para verificar entregas de x em x segundos

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { app } from "./app";
 
-app("PW769258045BR")
-    .then(() => {
-        console.log('Done');
-    })
-    .catch((err: Error) => {
-        console.log('Error: ' + err.message);
-    })
+app(["PW769258045BR"]);
